90c18fecef7129b41c61004569c2ca9dc3702235
Aymeric MASSE <aym.masse@gmail.com>
2017-06-16 18:10:14 -0400

    Transfer Utils functions into Little class
    All utils functions wre specific to the Little's algorithm. No need to
    separate them from the other.

--- Little.h
+++ Little.h
@@ -4,10 +4,12 @@
 #include <stack>
 #include <limits>
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 #include "LittleNode.h"
 #include "Segment.h"
-#include "Utils.h"
+#include "Coordinates.h"
 
 using namespace std;
 
@@ -36,6 +38,57 @@ public:
     void start();
 
     void examineNode(LittleNode node);
+
+    /**
+     * Return a vector of Coordinates containing the coordinates of all towers described inside fileName
+     * @param fileName the name of the file containing all the points coordinates
+     * @return a vector of coordinates
+     */
+    vector<Coordinates> getCoordinates(string fileName);
+
+    /**
+     * Return the matrix of distances between all points
+     * @param coordinates a vector of coordinates
+     * @return A matrix of doubles
+     */
+    MatrixXd *getDistancesMatrix(vector<Coordinates> coordinates);
+
+    /**
+     * Reduce a matrix row according to Little's algorithm
+     * @param matrix the matrix to reduce
+     * @param row the matrix's row index
+     * @return the value reduced
+     */
+    double reduceMatrixRow(MatrixXd *matrix, size_t row);
+
+    /**
+     * Reduce a matrix column according to Little's algorithm
+     * @param matrix the matrix to reduce
+     * @param col the matrix's column index
+     * @return the value reduced
+     */
+    double reduceMatrixCol(MatrixXd *matrix, size_t col);
+
+    /**
+     * Reduce all the matrix's row
+     * @param matrix the matrix
+     * @return the total amount reduced
+     */
+    double reduceMatrixRows(MatrixXd *matrix);
+
+    /**
+     * Reduce all the matrix's columns
+     * @param matrix the matrix
+     * @return the total amount reduced
+     */
+    double reduceMatrixCols(MatrixXd *matrix);
+
+    /**
+     * Reduce the matrix rows and columns
+     * @param matrix the matrix
+     * @return the total amount reduced
+     */
+    double reduceMatrix(MatrixXd *matrix);
 };
 
 
